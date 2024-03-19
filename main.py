9a6e3e38ab63781638be86445e9f83404f000c08
m.niestroj <mateusz.niestroj@equiqo.com>
2024-03-19 11:08:09 +0100

    count text words

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
 
+    words_count = count_words(text)
+
+    print(words_count)
+
+
+def count_words(text):
+    words = text.split()
+
+    return len(words)
 
 def get_book_text(path):
     with open(path) as f:
