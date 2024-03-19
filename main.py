c2e844dcc9dd30c8df1047c3eaabd684f2bc9950
m.niestroj <mateusz.niestroj@equiqo.com>
2024-03-19 12:13:19 +0100

    add: count chars

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ def main():
 
     words_count = count_words(text)
 
-    print(words_count)
+    print(f"{words_count} found in text")
+    chars_count = count_characters(text.lower())
+    print(f"Characters found in text: {chars_count}")
 
 
 def count_words(text):
@@ -16,5 +18,13 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def count_characters(text):
+    chars = {}
+    for char in text:
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars
 
 main()
